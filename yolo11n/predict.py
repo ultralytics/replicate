82cebe8044ba7880dd57a7f04c8d6e7edd70a9e7
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-08-06 20:42:02 +0200

    Parameterize push.yml (#22)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>

--- yolo11n/predict.py
+++ yolo11n/predict.py
@@ -1,9 +1,9 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-import json
 from typing import Optional
+
+from cog import BaseModel, BasePredictor, Input, Path
 from ultralytics import YOLO
-from cog import BasePredictor, Input, Path, BaseModel
 
 
 class Output(BaseModel):
@@ -32,9 +32,6 @@ class Predictor(BasePredictor):
         result.save(image_path)
 
         if return_json:
-            return Output(
-                image=Path(image_path), 
-                json_str=result.to_json()
-            )
+            return Output(image=Path(image_path), json_str=result.to_json())
         else:
             return Output(image=Path(image_path))
